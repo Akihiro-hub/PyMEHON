68e3c4709b929b8046e70b1b388eb4a782cac389
Akihiro-hub <tsukamoto.akihiro@kmcinc.co.jp>
2024-09-10 06:56:40 +0900

    Update AppProgramaPyMEs.py

--- AppProgramaPyMEs.py
+++ AppProgramaPyMEs.py
@@ -329,9 +329,13 @@ elif rubro == "Planificación de inventario":
 elif rubro == "Planificación de venta (Comedor)":
     st.write("## :blue[Planificación del monto de ventas en un comedor]") 
     st.write("###### El monto de la venta de un restaurante, comedor o cafetería se puede estimar, en base al número de asientos, aplicando esta calculadora.")  
+   
+    col1, col2 = st.columns(2)
+    with col1:
     a = st.number_input("¿Cuánto asientos tiene el comedor?", 0, 1000, 20)
     b = st.number_input("Tasa de ocupación de los asientos por los clientes (%)", 0, 100, 50)
     c = st.number_input("Veces estimadas de rotación de los clientes al día", 1, 10, 3)
+    with col2:
     d = st.number_input("Promedio estimado de la venta por cliente (Lps)", 1, 1000, 125)
     e = st.number_input("Días de operación al mes (Días)", 1, 31, 28)
     st.write("###### :red[La tasa de ocupación puede ser 50%, ya que sólo dos personas pueden ocupar la mesa para cuatro personas. La rotacion de los clientes al día puede ser 4 o 5 veces, como 2 rotaciones a horas de almuerzo y 2 rotaciones a horas de cena.]")
